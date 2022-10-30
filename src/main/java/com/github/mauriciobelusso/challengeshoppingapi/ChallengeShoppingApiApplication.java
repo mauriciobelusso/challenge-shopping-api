491ec32d108c6c361159dd58a7be1b37ddb819e6
mauriciobelusso <mauriciobelusso@gmail.com>
2022-10-30 00:42:57 -0300

    Initial commit

---
+++ src/main/java/com/github/mauriciobelusso/challengeshoppingapi/ChallengeShoppingApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.mauriciobelusso.challengeshoppingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeShoppingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeShoppingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/mauriciobelusso/challengeshoppingapi/ChallengeShoppingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mauriciobelusso.challengeshoppingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeShoppingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
